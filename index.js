fc7f511c4901247a3058c23193c8075190c90d8c
alisonkwork <156113052+alisonkwork@users.noreply.github.com>
2024-04-19 20:10:00 +0000

    Added information to each photo, added footer

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add JavaScript below
+
+ScrollReveal().reveal('.scroll1', {delay: 300, duration: 800});
+ScrollReveal().reveal('.scroll2', {delay: 310, duration: 900});
+ScrollReveal().reveal('.scroll3', {delay: 320, duration: 1000});
+ScrollReveal().reveal('.scroll4', {delay: 330, duration: 1100});
+ScrollReveal().reveal('.scroll5', {delay: 340, duration: 1200});
+ScrollReveal().reveal('.scroll6', {delay: 350, duration: 1300});
+ScrollReveal().reveal('.scroll7', {delay: 360, duration: 1300});
+ScrollReveal().reveal('.scroll8', {delay: 370, duration: 1300});
+ScrollReveal().reveal('.scroll9', {delay: 380, duration: 1300});
+ScrollReveal().reveal('.scroll10', {delay: 390, duration: 1300});
